a49bd5889887ad76cf91ab7a53f01cea05eca03e
Dan Sims <sims.daniel.ds@gmail.com>
2018-04-12 09:18:41 -0400

    calculate btn finished

---
+++ window-shopper/Controller/MainVC.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  window-shopper
+//
+//  Created by Dan Sims on 4/11/18.
+//  Copyright © 2018 Dan Sims. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        calcBtn.backgroundColor = #colorLiteral(red: 0.9700048575, green: 0.4979467592, blue: 0.1287249442, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    @objc func calculate() {
+        print("We got here")
+    }
+
+
+}
+
